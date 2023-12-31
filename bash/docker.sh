3fa28af9c0fe66a836ccff7f291dd54a0776bf09
AMJID DADER <amjiddader@gmail.com>
2023-12-31 16:02:22 +0530

    Update docker.sh

--- bash/docker.sh
+++ bash/docker.sh
@@ -1,16 +1,30 @@
-# /bin/bash
+#!/bin/bash
+
 echo "-------------------------"
 echo "    DOCKER INSTALLER     "
 echo "-------------------------"
 sleep 2
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
-sudo apt-get install ca-certificates curl gnupg
+
+# Remove existing Docker packages
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do
+  sudo apt-get remove $pkg -y
+done
+
+# Install prerequisites
+sudo apt-get install -y ca-certificates curl gnupg
+
+# Set up Docker GPG key
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+
+# Add Docker repository
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Update package list
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# Install Docker packages
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
