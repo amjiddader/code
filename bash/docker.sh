32d1c096fa5bc027f2a8bd9e3fd746b142cda59c
AMJID DADER <amjiddader@gmail.com>
2023-12-31 15:59:44 +0530

    Update and rename install.sh to docker.sh

--- bash/docker.sh
+++ bash/docker.sh
@@ -1,11 +1,13 @@
 # /bin/bash
-sudo apt-get update
+echo "-------------------------"
+echo "    DOCKER INSTALLER     "
+echo "-------------------------"
+sleep 2
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 sudo apt-get install ca-certificates curl gnupg
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-
-# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
